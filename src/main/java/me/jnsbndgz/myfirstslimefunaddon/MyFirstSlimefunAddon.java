1c217ccc40abe384f8d22ce96852e5946634cf16
jnsbndgz <jonasbendi2@gmail.com>
2022-01-12 23:04:40 +0100

    Added Fire Cake with functionality

--- src/main/java/me/jnsbndgz/myfirstslimefunaddon/MyFirstSlimefunAddon.java
+++ src/main/java/me/jnsbndgz/myfirstslimefunaddon/MyFirstSlimefunAddon.java
@@ -1,5 +1,7 @@
-package me.jnsbndgz.slimefunaddon;
+package me.jnsbndgz.myfirstslimefunaddon;
 
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
@@ -13,17 +15,21 @@ import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 
+import java.util.logging.Level;
+
 public class MyFirstSlimefunAddon extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
+        Bukkit.getLogger().log(Level.INFO, "My addon is loaded!");
         Config cfg = new Config(this);
 
         NamespacedKey categoryId = new NamespacedKey(this, "barbarian_technology");
-        CustomItemStack categoryItem = new CustomItemStack(Material.LEATHER_CHESTPLATE, "&4Barbarian Technology");
+        CustomItemStack categoryItem = new CustomItemStack(Material.WOODEN_AXE, "&4Barbarian Technology");
 
         ItemGroup itemGroup = new ItemGroup(categoryId, categoryItem);
-        SlimefunItemStack itemStack = new SlimefunItemStack("FIERY_AXE", Material.STONE_AXE, "&4Fiery Axe",
+
+        SlimefunItemStack FieryAxeItemStack = new SlimefunItemStack("FIERY_AXE", Material.STONE_AXE, "&4Fiery Axe",
                 "", "&cTurns your opponent", "&con fire!");
         ItemStack[] recipe = {
                 null,                                   new ItemStack(Material.BLAZE_POWDER),   null,
@@ -31,9 +37,21 @@ public class MyFirstSlimefunAddon extends JavaPlugin implements SlimefunAddon {
                 null,                                   new ItemStack(Material.BLAZE_POWDER),   null
         };
 
-        SlimefunItem sfItem = new SlimefunItem(itemGroup, itemStack, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
+        SlimefunItem sfItem = new SlimefunItem(itemGroup, FieryAxeItemStack, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
+
+        SlimefunItemStack FieryCakeItemStack = new SlimefunItemStack("FIRE_CAKE", Material.CAKE, "&cFire Cake",
+                "", "&cTurns you on fire!");
+
+        ItemStack[] FieryCakeRecipe = {
+                new ItemStack(Material.DIAMOND),    null,                               new ItemStack(Material.DIAMOND),
+                null,                               SlimefunItems.CARBONADO,            null,
+                new ItemStack(Material.DIAMOND),    null,                               new ItemStack(Material.DIAMOND)
+        };
+
+        FireCake fireCake = new FireCake(itemGroup, FieryCakeItemStack, RecipeType.ENHANCED_CRAFTING_TABLE, FieryCakeRecipe);
 
         sfItem.register(this);
+        fireCake.register(this);
     }
 
     @Override
